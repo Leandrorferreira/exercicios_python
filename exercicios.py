eb70d84b60a6b3058da4dc898cc53534b355a8ba
Leandrorferreira <leandrorferreira95@gmail.com>
2019-04-27 10:56:00 -0300

    Criar arquivo com exercicios

---
+++ exercicios.py
@@ -0,0 +1,41 @@
+#EXERCICIO 01
+# c = input('Qual material a ser usado para construir a escadinha? ')
+# qtd = int(input('Quantos degrais terá? '))
+# material = ""
+# while qtd >= 0:
+#     print(material)
+#     material += c
+#     qtd -= 1
+
+#EXERCICIO 02
+# import random
+
+# numero = int(random.random() * 10)
+
+# print(numero)
+# n = int(input('Adivinhe meu número: '))
+
+# if n == numero:
+#     print('Você acertou!')
+# elif n > numero:
+#     print(f'O número é menor {n}')
+# else:
+#     print(f'O número é maior que {n}')
+
+#EXERCICIO 03
+# texto = input('Digite uma palavra ')
+# #print(texto[::-1])
+# res = ""
+# for letra in texto:
+#     res = letra + res
+
+# print(res)
+
+#EXERCICIO 04
+# texto = input('Digite uma palavra ')
+# #print(texto[::-1])
+# res = ""
+# for letra in texto:
+#     res = letra + res
+
+# print(res)
